5ca9a2ef5381faed470032bf21a9072209e5ca06
MandipJoshii <mandip.joshi824@gmail.com>
2025-09-07 17:23:09 +0545

    some changes and begin doing numpy(for basic understanding)

---
+++ numpy/introduction/basic.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#one dimentional array
+marks = np.array([90,70,60,50,97,33,55])
+
+print(marks)
+
+# mark = [90,70,60,50,97,33,55]
+
+# print(mark)
+
+
+#/////////////////////////////////////////////
+
+#two dimentional array
+
+matrix = np.array([[1,2,3], [4,5,6],[7,8,9]])
+
+print(matrix)
