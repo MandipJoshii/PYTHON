f42db91309fb63f5c0c07c4bbfd6b5c48cb2cfc2
MandipJoshii <mandip.joshi824@gmail.com>
2025-09-17 17:01:40 +0545

    done: displaying the value, next: visualizing using matplotlib

--- numpy/projects/simulate_temperature/simulate_temperature.py
+++ numpy/projects/simulate_temperature/simulate_temperature.py
@@ -11,6 +11,23 @@ days = np.arange(365)
 seasonal_effects = (10 * np.sin(2 * np.pi * days/365))
 temps = temps + seasonal_effects 
 
-print(temps)
+print("\n",temps, "\n")
+
+date_range = pd.date_range(start = "2025-1-1", periods = 365 , freq = "D")
+
+print(date_range, "\n")
+
+data_frame = pd.DataFrame({
+    "DATE":date_range,  
+    "TEMPERATURE":temps
+    
+})
+print(data_frame, "\n")
+
+print("AVERAGE TEMPERATURE = ", data_frame["TEMPERATURE"].mean())
+print("STANDARD DEVIATION = ", data_frame["TEMPERATURE"].std())
+print("MAXIMUM TEMPERATURE = ", data_frame["TEMPERATURE"].max())
+print("MAXIMUM TEMPERATURE = ", data_frame["TEMPERATURE"].min())
+
 
 
